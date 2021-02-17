698f8852c5c0c7522197d9d7e0aab6cd95610562
Ma-189 <78057087+Ma-189@users.noreply.github.com>
2021-02-17 15:23:51 -0300

    Add files via upload

---
+++ src/ordenação_lg2/TesteRecursão.java
@@ -0,0 +1,15 @@
+package ordenação_lg2;
+
+public class TesteRecursão {
+
+	public static void main(String[] args) {
+		
+		int fatorialNR = Recursão.fatorialNaoRecursivo(5);
+		System.out.println(fatorialNR);
+		
+		int fatorialR = Recursão.fatorial(5);
+		System.out.println(fatorialR);
+		
+	}
+
+}
